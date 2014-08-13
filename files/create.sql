ae664434d44ed6fe644f77f6d1fa0a1c6fe2fd68
Eric Rochester <erochest@virginia.edu>
2014-08-13 16:25:47 +0000

    Added script to create the database.

---
+++ files/create.sql
@@ -0,0 +1,9 @@
+
+CREATE USER 'omeka'@'localhost' IDENTIFIED BY 'omeka';
+CREATE USER 'omeka'@'%' IDENTIFIED BY 'omeka';
+
+CREATE DATABASE omeka CHARACTER SET='utf8' COLLATE='utf8_unicode_ci';
+
+GRANT ALL PRIVILEGES ON omeka.* TO 'omeka'@'localhost';
+GRANT ALL PRIVILEGES ON omeka.* TO 'omeka'@'%';
+
