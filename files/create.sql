d98db149f256e4b50f70f2f3784a2e8eefcf20c1
Eric Rochester <erochest@virginia.edu>
2017-02-14 06:46:27 -0500

    Create databases for testing.

--- files/create.sql
+++ files/create.sql
@@ -7,3 +7,7 @@ CREATE DATABASE omeka CHARACTER SET='utf8' COLLATE='utf8_unicode_ci';
 GRANT ALL PRIVILEGES ON omeka.* TO 'omeka'@'localhost';
 GRANT ALL PRIVILEGES ON omeka.* TO 'omeka'@'%';
 
+CREATE DATABASE test_omeka CHARACTER SET='utf8' COLLATE='utf8_unicode_ci';
+
+GRANT ALL PRIVILEGES ON test_omeka.* TO 'omeka'@'localhost';
+GRANT ALL PRIVILEGES ON test_omeka.* TO 'omeka'@'%';
